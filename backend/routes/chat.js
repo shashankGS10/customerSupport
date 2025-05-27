8f1750c48eb22339f92215f5e2caa6f8a0a5b138
shashankGS10 <gsshashank@gmail.com>
2025-05-27 14:12:36 +0530

    GPT+Supabase Dev tested

--- backend/routes/chat.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { sendChat, getHistory } = require('../controllers/chatController');
-const protect = require('../middleware/authMiddleware');
-
-router.post('/send', protect, sendChat);
-router.get('/history', protect, getHistory);
-
-module.exports = router;
